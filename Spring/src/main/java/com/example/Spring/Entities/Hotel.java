0381e01aeee02c9253b607ff99bc4ab718b3cdca
Nevena Cuturilov <cuturilovnevena@gmail.com>
2018-12-27 03:36:59 +0100

    Dodate neke osnovne klase.

---
+++ Spring/src/main/java/com/example/Spring/Entities/Hotel.java
@@ -0,0 +1,4 @@
+package com.example.Spring.Entities;
+
+public class Hotel {
+}
---
+++ Spring/src/main/java/com/example/Spring/Entities/Korisnik.java
@@ -0,0 +1,8 @@
+package com.example.Spring.Entities;
+
+public class Korisnik {
+    private String username;
+    private String password;
+
+
+}
